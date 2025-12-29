2667e9d9c1815407da0e33ea56d38971a17d1f94
Ella <ella.tseluiko@gmail.com>
2025-12-29 11:53:37 -0600

    Update code

---
+++ src/js/1-timer.js
@@ -0,0 +1,88 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const datetimePicker = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+let selectedDate = null;
+let timerId = null;
+
+startBtn.disabled = true;
+
+flatpickr(datetimePicker, {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+
+  onClose(selectedDates) {
+    const pickedDate = selectedDates[0];
+
+    if (pickedDate <= new Date()) {
+      startBtn.disabled = true;
+      selectedDate = null;
+      iziToast.error({
+        position: 'topRight',
+        message: 'Please choose a date in the future',
+      });
+      return;
+    }
+
+    selectedDate = pickedDate;
+    startBtn.disabled = false;
+  },
+});
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  datetimePicker.disabled = true;
+
+  timerId = setInterval(() => {
+    const deltaTime = selectedDate - new Date();
+
+    if (deltaTime <= 0) {
+      clearInterval(timerId);
+      updateTimer(0);
+      datetimePicker.disabled = false;
+      startBtn.disabled = true;
+      return;
+    }
+
+    updateTimer(deltaTime);
+  }, 1000);
+});
+
+function updateTimer(ms) {
+  const { days, hours, minutes, seconds } = convertMs(ms);
+
+  daysEl.textContent = addLeadingZero(days);
+  hoursEl.textContent = addLeadingZero(hours);
+  minutesEl.textContent = addLeadingZero(minutes);
+  secondsEl.textContent = addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  return {
+    days: Math.floor(ms / day),
+    hours: Math.floor((ms % day) / hour),
+    minutes: Math.floor((ms % hour) / minute),
+    seconds: Math.floor((ms % minute) / second),
+  };
+}
